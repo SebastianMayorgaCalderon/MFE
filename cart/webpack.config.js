f28bfbd27b3f7a95878a64a06c51e7c5263f6564
Sebatian <sebasmayorga7@gmail.com>
2021-10-06 13:49:18 -0700

    first commit

---
+++ cart/webpack.config.js
@@ -0,0 +1,10 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8082,
+  },
+  plugins: []
+};
